8770c0d82eb23ac7033e35527a8fd24997f47cdf
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2022-04-11 10:00:20 -0300

    curso

--- controller/cursoController.js
+++ controller/cursoController.js
@@ -2,32 +2,39 @@ const { Op } = require("sequelize");
 const Curso = require("../models/Curso");
 
 async function abreadd(req, res) {
-  res.render("add.ejs", {});
+  res.render("curso/add.ejs", {});
 }
 
 //alterar dados
 async function add(req, res) {
-  const { nome, email, senha } = req.body;
-  const foto = req.file.filename;
-  await Curso.create({ nome, email, senha, foto }).then((curso) => {
+  const { titulo, descricao, cargahoraria, datainicio } = req.body;
+  const logo = req.file.filename;
+  await Curso.create({
+    titulo,
+    descricao,
+    cargahoraria,
+    datainicio,
+    logo,
+  }).then((curso) => {
     res.redirect("/");
   });
 }
 
 async function abreedt(req, res) {
   let curso = await Curso.findByPk(req.params.id);
-  res.render("edt.ejs", { curso: curso });
+  res.render("curso/edt.ejs", { curso: curso });
 }
 
 //alterar dados
 async function edt(req, res) {
   let curso = await Curso.findByPk(req.params.id);
-  curso.nome = req.body.nome;
-  curso.email = req.body.email;
-  curso.senha = req.body.senha;
+  curso.titulo = req.body.titulo;
+  curso.descricao = req.body.descricao;
+  curso.cargahoraria = req.body.cargahoraria;
+  curso.datainicio = req.body.datainicio;
 
   if (req.file.filename != "") {
-    curso.foto = req.file.filename;
+    curso.logo = req.file.filename;
   }
   await curso.save();
   res.redirect("/");
@@ -35,16 +42,16 @@ async function edt(req, res) {
 
 async function list(req, res) {
   let cursos = await Curso.findAll();
-  res.render("index.ejs", { Cursos: cursos });
+  res.render("curso/index.ejs", { Cursos: cursos });
 }
 
 //pode ter alteração
 async function listfiltro(req, res) {
   let pesquisar = req.body.pesquisar;
   let cursos = await Curso.findAll({
-    where: { nome: { [Op.like]: "%" + pesquisar + "%" } },
+    where: { titulo: { [Op.like]: "%" + pesquisar + "%" } },
   });
-  res.render("index.ejs", { Cursos: cursos });
+  res.render("curso/index.ejs", { Cursos: cursos });
 }
 
 async function del(req, res) {
--- controller/usuarioController.js
+++ controller/usuarioController.js
@@ -2,7 +2,7 @@ const { Op } = require("sequelize");
 const Usuario = require("../models/Usuario");
 
 async function abreadd(req, res) {
-  res.render("add.ejs", {});
+  res.render("usuario/add.ejs", {});
 }
 
 async function add(req, res) {
@@ -15,7 +15,7 @@ async function add(req, res) {
 
 async function abreedt(req, res) {
   let usuario = await Usuario.findByPk(req.params.id);
-  res.render("edt.ejs", { usuario: usuario });
+  res.render("usuario/edt.ejs", { usuario: usuario });
 }
 
 async function edt(req, res) {
@@ -33,7 +33,7 @@ async function edt(req, res) {
 
 async function list(req, res) {
   let usuarios = await Usuario.findAll();
-  res.render("index.ejs", { Usuarios: usuarios });
+  res.render("usuario/index.ejs", { Usuarios: usuarios });
 }
 
 async function listfiltro(req, res) {
@@ -41,7 +41,7 @@ async function listfiltro(req, res) {
   let usuarios = await Usuario.findAll({
     where: { nome: { [Op.like]: "%" + pesquisar + "%" } },
   });
-  res.render("index.ejs", { Usuarios: usuarios });
+  res.render("usuario/index.ejs", { Usuarios: usuarios });
 }
 
 async function del(req, res) {
